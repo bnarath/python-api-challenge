8af529cf0736e0f1e652435d2b5b723c8241ea0c
Bincy Narath <bincybaburajnarath@gmail.com>
2020-06-16 12:04:25 -0400

    Finished WeatherPy code

--- CodeBase/api_keys.py
+++ CodeBase/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "34ad886c1b9446d2149cd08d68604099"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBeJUX9f6OR149H_RJu-CTftpG6fJ9rOW8"
